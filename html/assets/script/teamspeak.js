a740c1fa64ae8596a765926fe6f0f3e406220876
Dunkelmann <business@dunkelmann.eu>
2021-11-28 15:27:17 +0100

    Changed some 'special' things

--- html/assets/script/teamspeak.js
+++ html/assets/script/teamspeak.js
@@ -1,29 +1,13 @@
-var index = 0;
-var colorArr = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
-'#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-'#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
-'#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-'#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
-'#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-'#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
-'#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-'#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
-'#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
-function flashtext() {
-
-    var el = document.querySelectorAll('*');
-    for(var i=0;i<el.length;i++){
-el[i].style.setProperty("color", colorArr[index], "important");
-}
-  index++;
-  if(index>colorArr.length) {index = 0;}
-}
-
-
 
 jebaited=function(){
     var vid = document.getElementById('vid');
     vid.play();
+
+    var el = document.querySelectorAll("div,table,p,code,tr,td,th");
+    for(var i=0;i<el.length;i++){
+el[i].className += " special";
+}
+
     var all = document.getElementsByClassName('bg-primary');
 for (var i = 0; i < all.length; i++) {
 all[i].style.setProperty("background-image", "url(assets/special/rainbow.gif)", "important");
@@ -36,9 +20,6 @@ var all = document.getElementsByClassName('btn-danger');
 for (var i = 0; i < all.length; i++) {
 all[i].style.setProperty("background-image", "url(assets/special/rainbow.gif)", "important");
 }
-    setInterval(function() {
-        flashtext();
-    }, 250 );
 }
 
 let TS3_EP = "aHR0cHM6Ly9hcGkuZHVua2VsbWFubi5ldS90czN2ZXJzaW9u";
