0da35f6d959d079226441c67d476329eba190c57
Dunkelmann <business@dunkelmann.eu>
2021-12-04 10:59:57 +0100

    Add minified CSS + Copyright notice

--- html/assets/script/discovery.js
+++ html/assets/script/discovery.js
@@ -1,3 +1,16 @@
+/*! *****************************************************************************
+Copyright (c) Dunkelmann 2021. All rights reserved.
+
+This code is licensed under the BSD License
+You should have received a copy of the BSD License
+with this code and it is also available through
+the world-wide-web at <https://files.dunkelmann.eu/license.txt>.
+If you did not receive a copy of the BSD License
+and are unable to obtain it through the world-wide-web,
+please send a note to <business@dunkelmann.eu> so we can mail you a copy
+immediately.
+***************************************************************************** */
+
 let DISCOVERY_ENDPOINT = "aHR0cHM6Ly9jb3JzLXByb3h5LmR1bmtlbG1hbm4uZXUvNjg3NDc0NzA3MzNhMmYyZjY0Njk3MzYzNmY3NjY1NzI3OTJlNzQ2NTYxNmQ3MzcwNjU2MTZiMmU2MzZmNmQyZjcxNzU2NTcyNzkyZi8=";
 decodeBase64=function(f){var g={},b=65,d=0,a,c=0,h,e="",k=String.fromCharCode,l=f.length;for(a="";91>b;)a+=k(b++);a+=a.toLowerCase()+"0123456789+/";for(b=0;64>b;b++)g[a.charAt(b)]=b;for(a=0;a<l;a++)for(b=g[f.charAt(a)],d=(d<<6)+b,c+=6;8<=c;)((h=d>>>(c-=8)&255)||a<l-2)&&(e+=k(h));return e};
 
