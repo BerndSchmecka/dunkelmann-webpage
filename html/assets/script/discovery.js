c37423513f8fc8a75b07aa905559f73332b537cf
Dunkelmann <business@dunkelmann.eu>
2021-12-14 22:34:12 +0100

    Reimplement Discovery with Vue

--- html/assets/script/discovery.js
+++ html/assets/script/discovery.js
@@ -26,72 +26,64 @@ parseUnixTime=function(t){
     return formattedTime;
 }
 
-discoveryCard=function(obj){
-    return `<div class="col-md-auto">
-                <div class="discovery-object">
-                    <div class="discovery-banner" style="background: linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(39,39,200,1) 33%, rgba(2,117,215,1) 63%, rgba(0,212,255,1) 100%);"></div>
-                    <div class="discovery-icon">
-                        <span style="background: url(${"assets/discovery/icon_placeholder.png"}); background-size: 64px 64px;"></span>
-                    </div>
-                    <div class="discovery-name">
-                        <div class="discovery-name-text" title="${obj.name}"> ${obj.name} </div>
-                    </div>
-                    <div class="discovery-people">
-                        <div class="discovery-people-icon"><i class="fas fa-user fa-discovery-icon"></i></div>
-                        <div class="discovery-small-text discovery-people-text"> ${obj.members + (obj.type.toLowerCase() === "server" ? " - " + obj.address : "") } </div>
-                    </div>
-                    <div class="discovery-desc">
-                        <div class="discovery-small-text"> ${obj.topic ?? "No description available"} </div>
-                    </div>
-                    <div class="discovery-created discovery-small-text"> First seen: ${parseUnixTime(obj.created)} </div>
-                    <div class="discovery-footer">
-                        <div class="discovery-connect">
-                            <div class="discovery-small-text discovery-link" title="${obj.type.toLowerCase() === "server" ? "Open a connection to this server" : "Join this " + obj.type.toLowerCase()}"> Join ${obj.type.toLowerCase()} </div>
-                        </div>
-                        ${
-                            obj.type.toLowerCase() === "server" ? 
-                            `<div class="discovery-bookmark" title="Bookmark this server">
-                                <i class="fas fa-bookmark fa-bookmark-icon"></i>
-                            </div>
-                            <div class="discovery-channel" title="${obj.canCreateChannel ? "Guests can create channels" : "Guests can't create channels"}">
-                                <i class="fas fa-plus-square ${obj.canCreateChannel ? "fa-channel-icon text-primary" : "fa-channel-icon"}"></i>
-                            </div>
-                            <div class="discovery-homebase" title="${obj.canCreateHomebase ? "Guests can use this server as homebase" : "Guests can't use this server as homebase"}">
-                                <i class="fas fa-home ${obj.canCreateHomebase ? "fa-homebase-icon text-primary" : "fa-homebase-icon"}"></i>
-                            </div>`
-                            : ""
-                        }
-                    </div>
-                </div>
-            </div>`
-}
-
-doQuery=function(q){
-    var query = new XMLHttpRequest();
-    query.onreadystatechange = function() {
-        if(query.readyState === 4 && query.status === 200){
-            var obj = JSON.parse(query.responseText);
-
-            document.getElementById('results').innerHTML = "";
-
-            obj.entries.forEach(element => {
-                document.getElementById('results').innerHTML += discoveryCard(element);
-            });
+var DiscoveryCards = new Vue({
+    el: '#cardsDiv',
+    data: {
+        cards: []
+    },
+    created: function() {
+        this.doQuery("*%3A*");
+    },
+    methods: {
+        doQuery: function(q) {
+            var query = new XMLHttpRequest();
+            query.onreadystatechange = function() {
+                if(query.readyState === 4 && query.status === 200){
+                    var obj = JSON.parse(query.responseText);
+        
+                    DiscoveryCards.cards = [];
+        
+                    obj.entries.forEach(element => {
+                        DiscoveryCards.cards.push({
+                            name: element.name,
+                            topic: element.topic ?? 'No description available',
+                            people: element.members + (element.type.toLowerCase() === 'server' ? ' - ' + element.address : ''),
+                            created: `First seen: ${parseUnixTime(element.created)}`,
+                            join: `Join ${element.type.toLowerCase()}`,
+                            tooltips: {
+                                join: element.type.toLowerCase() === 'server' ? 'Open a connection to this server' : `Join this ${element.type.toLowerCase()}`,
+                                channel: element.canCreateChannel ? 'Guests can create channels' : "Guests can't create channels",
+                                homebase: element.canCreateHomebase ? 'Guests can use this server as homebase' : "Guests can't use this server as homebase"
+                            },
+                            classes: {
+                                channel: `fas fa-plus-square ${element.canCreateChannel ? "fa-channel-icon text-primary" : "fa-channel-icon"}`,
+                                homebase: `fas fa-home ${element.canCreateHomebase ? "fa-homebase-icon text-primary" : "fa-homebase-icon"}`
+                            },
+                            isServer: element.type.toLowerCase() === 'server'
+                        });
+                    });
+                }
+            }
+            query.open("GET", decodeBase64(DISCOVERY_ENDPOINT) + "?q=" + q + "&start=0&rows=30&sort_by=members&sort_order=desc");
+            query.send();
         }
     }
-    query.open("GET", decodeBase64(DISCOVERY_ENDPOINT) + "?q=" + q + "&start=0&rows=30&sort_by=members&sort_order=desc");
-    query.send();
-}
+});
 
-const node = document.getElementById("searchbox");
-const filter = document.getElementById("filter");
-node.addEventListener("keyup", ({key}) => {
-    if (key === "Enter") {
-        doQuery((node.value ? `%2B*${encodeURIComponent(node.value)}*` : "*%3A*") + filter.value);
+var control = new Vue({
+    el: '#control_elements',
+    data: {
+        nodeValue: '',
+        filterValue: ''
+    },
+    methods: {
+        queryWithFilter: function() {
+            DiscoveryCards.doQuery((this.nodeValue ? `%2B*${encodeURIComponent(this.nodeValue)}*` : "*%3A*") + this.filterValue);
+        }
+    },
+    watch: {
+        filterValue: function() {
+            this.queryWithFilter();
+        }
     }
 });
-filter.addEventListener("change", () => {
-    doQuery((node.value ? `%2B*${encodeURIComponent(node.value)}*` : "*%3A*") + filter.value);
-});
-
-doQuery("*%3A*");
