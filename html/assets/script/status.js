e8942fb69f79d871806d516ae4189866f3eb9170
Dunkelmann <business@dunkelmann.eu>
2021-12-15 10:32:06 +0100

    Add Vue to status as well

--- html/assets/script/status.js
+++ html/assets/script/status.js
@@ -14,33 +14,67 @@ immediately.
 let STATUS_URL = "aHR0cHM6Ly9hcGkuZHVua2VsbWFubi5ldS9zdGF0dXM=";
 decodeBase64=function(f){var g={},b=65,d=0,a,c=0,h,e="",k=String.fromCharCode,l=f.length;for(a="";91>b;)a+=k(b++);a+=a.toLowerCase()+"0123456789+/";for(b=0;64>b;b++)g[a.charAt(b)]=b;for(a=0;a<l;a++)for(b=g[f.charAt(a)],d=(d<<6)+b,c+=6;8<=c;)((h=d>>>(c-=8)&255)||a<l-2)&&(e+=k(h));return e};
 
-			function appendImage(obj, name) {
-			document.getElementById(obj.id).innerHTML += "<td><i class=\"fas fa-circle text-" + name + "\"></i> "+obj.statusCode+" "+obj.statusDesc+"</td>"
+let dict = {
+	'website': 'Homepage',
+	'api': 'Dunkelmann-API',
+	'matrix': 'Chat-Server (Synapse)',
+	'insomnium': 'Chat-Server (Insomnium)',
+	'mail': 'Mail-Server (Postfix)',
+	'cloud': 'Dunkelmann-Cloud'
+};
+
+Vue.component('status-service', {
+	props: ['status'],
+	template: `
+	<tr :id="status.id">
+	<th scope="col">{{ status.name }}</th>
+	<td>{{ status.id }}</td>
+	<td>{{ status.url }}</td>
+	<td><i :class="status.status.classes"></i> {{ status.status.message }}</td>
+	</tr>
+	`
+});
+
+var app = new Vue({
+	el: '#statusApp',
+	data: {
+		services: []
+	},
+	created: function() {
+		this.requestData();
+	},
+	methods: {
+		getClassText: function(code) {
+			if(200 <= code && code <= 299){
+				return "success";
+			} else if(300 <= code && code <= 399) {
+				return "redirection";
+			} else if(400 <= code && code <= 499) {
+				return "warning";
+			} else if(500 <= code && code <= 599) {
+				return "danger";
+			} else if(600 <= code && code <= 699) {
+				return "primary";
+			} else {
+				return "secondary";
 			}
-			
+		},
+		requestData: function() {
 			var req = new XMLHttpRequest();
 			req.onreadystatechange = function() {
 			if(req.readyState === 4 && req.status === 200){
 			var resp = JSON.parse(req.responseText);
 			
 			resp.forEach(function(obj) {
-			document.getElementById(obj.id).innerHTML += "<td>"+obj.id+"</td>"
-								  +  "<td>"+obj.url+"</td>";
-			
-			if(200 <= obj.statusCode && obj.statusCode <= 299){
-			appendImage(obj, "success");
-			} else if(300 <= obj.statusCode && obj.statusCode <= 399) {
-			appendImage(obj, "redirection");
-			} else if(400 <= obj.statusCode && obj.statusCode <= 499) {
-			appendImage(obj, "warning");
-			} else if(500 <= obj.statusCode && obj.statusCode <= 599) {
-			appendImage(obj, "danger");
-			} else if(600 <= obj.statusCode && obj.statusCode <= 699) {
-			appendImage(obj, "primary");
-			} else {
-			appendImage(obj, "secondary");
-			}
-			
+				app.services.push({
+					name: dict[obj.id],
+					id: obj.id,
+					url: obj.url,
+					status: {
+						classes: `fas fa-circle text-${app.getClassText(obj.statusCode)}`,
+						message: `${obj.statusCode} ${obj.statusDesc}`
+					}
+				});
 			});
 			
 			} else if (req.readyState === 4) {
@@ -49,3 +83,6 @@ decodeBase64=function(f){var g={},b=65,d=0,a,c=0,h,e="",k=String.fromCharCode,l=
 			}
 			req.open("GET", decodeBase64(STATUS_URL));
 			req.send();
+		}
+	}
+});
