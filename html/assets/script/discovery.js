e60885effc163d7888e9c13b45fe40c18deb655e
Lucas Neuber <Lucas-Juergen.Neuber@student.tu-freiberg.de>
2022-08-22 14:27:06 +0200

    Fix Discovery

--- html/assets/script/discovery.js
+++ html/assets/script/discovery.js
@@ -11,26 +11,16 @@ please send a note to <business@dunkelmann.eu> so we can mail you a copy
 immediately.
 ***************************************************************************** */
 
-let DISCOVERY_ENDPOINT = "aHR0cHM6Ly9jb3JzLXByb3h5LmR1bmtlbG1hbm4uZXUvNjg3NDc0NzA3MzNhMmYyZjY0Njk3MzYzNmY3NjY1NzI3OTJlNzQ2NTYxNmQ3MzcwNjU2MTZiMmU2MzZmNmQyZjcxNzU2NTcyNzkyZi8=";
-let DEV_DISCOVERY_ENDPOINT = "aHR0cHM6Ly9jb3JzLXByb3h5LmR1bmtlbG1hbm4uZXUvNjg3NDc0NzA3MzNhMmYyZjY0NjU3NjJkNjQ2OTczNjM2Zjc2NjU3Mjc5MmU3NDY1NjE2ZDczNzA2NTYxNmIyZTYzNmY2ZDJmNzE3NTY1NzI3OTJmLw==";
-let DUNKELMANN_DISCOVERY_ENDPOINT = "aHR0cHM6Ly9kaXNjb3ZlcnkuZHVua2VsbWFubi5ldS8=";
+let DISCOVERY_ENDPOINT = "aHR0cHM6Ly9hcGkuZHVua2VsbWFubi5ldS92MS9nZXRUZWFtU3BlYWtSb29tRGlzY292ZXJ5";
 decodeBase64=function(f){var g={},b=65,d=0,a,c=0,h,e="",k=String.fromCharCode,l=f.length;for(a="";91>b;)a+=k(b++);a+=a.toLowerCase()+"0123456789+/";for(b=0;64>b;b++)g[a.charAt(b)]=b;for(a=0;a<l;a++)for(b=g[f.charAt(a)],d=(d<<6)+b,c+=6;8<=c;)((h=d>>>(c-=8)&255)||a<l-2)&&(e+=k(h));return e};
 
-getEndpoint=function(){
+getEndpointMode=function(){
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     if(urlParams.has('mode')) {
-        if(urlParams.get('mode') === 'dev'){
-            return DEV_DISCOVERY_ENDPOINT;
-        } else if (urlParams.get('mode') === 'prod') {
-            return DISCOVERY_ENDPOINT;
-        } else if (urlParams.get('mode') === 'dunkelmann') {
-            return DUNKELMANN_DISCOVERY_ENDPOINT;
-        } else {
-            return DISCOVERY_ENDPOINT;
-        }
+        return urlParams.get('mode');
     } else {
-        return DISCOVERY_ENDPOINT;
+        return "tsProd";
     }
 }
 
@@ -185,8 +175,18 @@ var app = new Vue({
                     });
                 }
             }
-            query.open("GET", decodeBase64(getEndpoint()) + "?q=" + q + "&start=0&rows=30&sort_by=members&sort_order=desc");
-            query.send();
+            query.open("POST", decodeBase64(DISCOVERY_ENDPOINT));
+            query.setRequestHeader("Content-type", "application/json");
+            query.send(
+                JSON.stringify({
+                    "endpointMode": getEndpointMode(),
+                    "queryString": q,
+                    "startCount": 0,
+                    "rowCount": 30,
+                    "sortBy": "members",
+                    "sortOrder": "desc"
+                })
+            );
         }
     },
     watch: {
