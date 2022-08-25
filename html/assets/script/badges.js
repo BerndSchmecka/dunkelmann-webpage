f92385818d45f8bf56aea971414162c2d4b54544
Lucas Neuber <Lucas-Juergen.Neuber@student.tu-freiberg.de>
2022-08-25 22:07:58 +0200

    Add ability to switch to cached revisions (badges)

--- html/assets/script/badges.js
+++ html/assets/script/badges.js
@@ -12,6 +12,7 @@ immediately.
 ***************************************************************************** */
 
 let BADGES_EP = "aHR0cHM6Ly9hcGkuZHVua2VsbWFubi5ldS92Mi9nZXRUZWFtU3BlYWtCYWRnZXM=";
+let REVISIONS_EP = "aHR0cHM6Ly9hcGkuZHVua2VsbWFubi5ldS92Mi9nZXRDYWNoZWRCYWRnZVJldmlzaW9ucw==";
 decodeBase64=function(f){var g={},b=65,d=0,a,c=0,h,e="",k=String.fromCharCode,l=f.length;for(a="";91>b;)a+=k(b++);a+=a.toLowerCase()+"0123456789+/";for(b=0;64>b;b++)g[a.charAt(b)]=b;for(a=0;a<l;a++)for(b=g[f.charAt(a)],d=(d<<6)+b,c+=6;8<=c;)((h=d>>>(c-=8)&255)||a<l-2)&&(e+=k(h));return e};
 
 parseUnixTime=function(t){
@@ -57,14 +58,36 @@ Vue.component('badge-card', {
 var app = new Vue({
     el: '#badgesApp',
     data: {
+        revisionValue: 'latest',
+        revisionList: [],
         rev: 0,
         lastMod: 0,
         cards: []
     },
     created: function() {
         this.queryBadges();
+        this.getCachedRevisions();
     },
     methods: {
+        getCachedRevisions: function() {
+            var query = new XMLHttpRequest();
+            query.onreadystatechange = function() {
+                if(query.readyState === 4 && query.status === 200){
+                    var obj = JSON.parse(query.responseText);
+
+                    app.revisionList = [];
+
+                    obj.cachedRevisions.forEach(e => {
+                        app.revisionList.push({
+                            name: e,
+                            id: e
+                        });
+                    });
+                }
+            }
+            query.open("GET", decodeBase64(REVISIONS_EP), true);
+            query.send();
+        },
         queryBadges: function() {
             var query = new XMLHttpRequest();
             query.onreadystatechange = function() {
@@ -90,7 +113,7 @@ var app = new Vue({
             query.open("POST", decodeBase64(BADGES_EP));
             query.send(
                 JSON.stringify({
-                    "revision": "latest"
+                    "revision": this.revisionValue
                 })
             );
         },
@@ -104,6 +127,11 @@ var app = new Vue({
             var seconds = "0" + date.getSeconds();
             var formattedTime = curr_date.substr(-2) + '.' + curr_month.substr(-2)  + '.' + curr_year + ' - ' + hours.substr(-2) + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
             return formattedTime;
+        }
+    },
+    watch: {
+        revisionValue: function() {
+            this.queryBadges();
         }
     }
 });
