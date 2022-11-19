dc471b9d790a01f29f9ade0e25be1d8c2be67d6b
Lucas Neuber <Lucas-Juergen.Neuber@student.tu-freiberg.de>
2022-11-19 16:28:18 +0100

    Fix viewer

--- html/assets/script/teamspeak.js
+++ html/assets/script/teamspeak.js
@@ -18,6 +18,25 @@ let ALPHA_URL = "";
 
 let TS5_URL = "aHR0cHM6Ly93d3cudGVhbXNwZWFrLmNvbS9kZS9kb3dubG9hZHMvI3RzNQ==";
 
+// Selecting the iframe element
+var frame = document.getElementById("viewerFrame");
+          
+// Adjusting the iframe height onload event
+frame.onload = function()
+// function execute while load the iframe
+{
+    // set the height of the iframe as 
+    // the height of the iframe content
+    frame.style.height = 
+    frame.contentWindow.document.body.scrollHeight + 'px';
+           
+    // set the width of the iframe as the 
+    // width of the iframe content
+    frame.style.width  = 
+    frame.contentWindow.document.body.scrollWidth+'px';
+              
+}
+
 requestJSON = function(version, endpoint, callback){
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
