1164b5427a9305df8660b4b7416dc095f1e69e39
Lucas Neuber <Lucas-Juergen.Neuber@student.tu-freiberg.de>
2022-11-19 21:24:51 +0100

    Fix PHP

--- html/assets/dynamic/cached_badge.php
+++ html/assets/dynamic/cached_badge.php
@@ -6,35 +6,36 @@ $rev = $_GET['rev'];
 
 // Get the badge image from https://api.dunkelmann.eu/v2/downloadCachedBadgeImage via POST a JSON object
 // with the query parameters 'uuid', 'variant' and 'rev'
+// Also save the response code to $httpcode
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://api.dunkelmann.eu/v2/downloadCachedBadgeImage");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array('uuid' => $uuid, 'variant' => $variant, 'revision' => $rev)));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$server_output = curl_exec($ch);
-curl_close ($ch);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+$badge = curl_exec($ch);
+$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
 
-// Output the badge image
-// Set the content type depending on the variant
-// svg -> image/svg+xml
-// svg_detailed -> image/svg+xml
-// png_16 -> image/png
-// png_64 -> image/png
-// If the curl request did not return 200 OK, output the json object
-
-if ($ch.http_code == 200) {
-    if ($variant == "svg") {
-        header("Content-Type: image/svg+xml");
-    } else if ($variant == "svg_detailed") {
-        header("Content-Type: image/svg+xml");
-    } else if ($variant == "png_16") {
-        header("Content-Type: image/png");
-    } else if ($variant == "png_64") {
-        header("Content-Type: image/png");
+// If the response code is 200, the badge image was successfully retrieved
+if ($httpcode == 200) {
+    // Set the content type depending on the variant
+    // svg -> image/svg+xml
+    // svg_detailed -> image/svg+xml
+    // png_16 -> image/png
+    // png_64 -> image/png
+    if ($variant == "svg" || $variant == "svg_detailed") {
+        header('Content-Type: image/svg+xml');
+    } else if ($variant == "png_16" || $variant == "png_64") {
+        header('Content-Type: image/png');
     }
-    echo $server_output;
+    // Output the badge image
+    echo $badge;
 } else {
-    header("Content-Type: application/json");
-    echo $server_output;
+    // If the response code is not 200, the badge image could not be retrieved
+    // Set the content type to application/json
+    header('Content-Type: application/json');
+    // Output the response code
+    echo $badge;
 }
 ?>
