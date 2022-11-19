0759ebbbffbd94da1eb55d83c3eed6f063e540a6
Lucas Neuber <Lucas-Juergen.Neuber@student.tu-freiberg.de>
2022-11-19 21:16:38 +0100

    Add Content-Type

--- html/assets/dynamic/cached_badge.php
+++ html/assets/dynamic/cached_badge.php
@@ -6,22 +6,34 @@ $rev = $_GET['rev'];
 
 // Get the badge image from https://api.dunkelmann.eu/v2/downloadCachedBadgeImage via POST a JSON object
 // with the query parameters 'uuid', 'variant' and 'rev'
+// Also save the Content-Type header in $contentType
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://api.dunkelmann.eu/v2/downloadCachedBadgeImage");
+curl_setopt($ch, CURLOPT_URL, 'https://api.dunkelmann.eu/v2/downloadCachedBadgeImage');
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array('uuid' => $uuid, 'variant' => $variant, 'revision' => $rev)));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$server_output = curl_exec($ch);
-curl_close ($ch);
+curl_setopt($ch, CURLOPT_HEADER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+$response = curl_exec($ch);
+$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+$header = substr($response, 0, $header_size);
+$body = substr($response, $header_size);
+curl_close($ch);
 
-// Output the badge image if the request was successful (HTTP status code 200)
-// Else output the json error message
-if (strpos($http_response_header[0], '200') !== false) {
-    header('Content-Type: image/png');
-    echo $server_output;
-} else {
-    header('Content-Type: application/json');
-    echo $server_output;
+// Get the Content-Type header
+$contentType = '';
+foreach (explode("\r
+
+", $header) as $line) {
+    if (strpos($line, 'Content-Type:') === 0) {
+        $contentType = substr($line, 14);
+    }
 }
+
+// Set the Content-Type header
+header('Content-Type: ' . $contentType);
+
+// Output the badge image
+echo $body;
 
 ?>
